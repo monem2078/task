f4ebced748546a287466f294464bf8ff53c62442
monem2078 <you@example.com>
2019-11-09 15:28:33 +0200

    implement reverse algorithm to command line tool

--- app/command.php
+++ app/command.php
@@ -1,7 +1,9 @@
 <?php
 use shiftAlgorithm\shiftAlgorithm;
+use reverseAlgorithm\reverseAlgorithm;
 
 include(__DIR__.'/shiftAlgorithm.php');
+include(__DIR__.'/reverseAlgorithm.php');
 // as the first argument is the file name.
 unset($argv[0]);
 $result = "";
@@ -11,7 +13,7 @@ if ($argc == 4) {
         if ($argv[3] == "encrypt") {
             $result = $command->shiftEncryptionAlgorithm($argv[1]);
         } elseif ($argv[3] == "decrypt") {
-            $result = $command->shiftDecryptionAlgorithm($argv[1]);
+            $result = $command->shiftEncryptionAlgorithm($argv[1]);
         } else {
             print "Enter the method to be used";
         }
@@ -20,6 +22,17 @@ if ($argc == 4) {
             //do some code.
         } elseif ($argv[3] == "decrypt") {
             //do some code.
+        } else {
+            print "Enter the method to be used";
+        }
+    } elseif ($argv[2] == "reverse_algorithm") {
+        $command = new reverseAlgorithm();
+        if ($argv[3] == "encrypt") {
+            $result = $command->reverseAlgorithmEncryption($argv[1]);
+            $result = substr($result, 11, 11);
+        } elseif ($argv[3] == "decrypt") {
+            $result = $command->reverseAlgorithmDecryption($argv[1]);
+            $result = substr($result, 11, 11);
         } else {
             print "Enter the method to be used";
         }
